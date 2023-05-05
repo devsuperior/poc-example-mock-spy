1901a60a6dd10489419ef192a6b79b65b6bec8bd
oliveiralex <alexandre.si.ufu@gmail.com>
2023-05-05 10:41:14 -0300

    Project created

---
+++ src/main/java/com/devsuperior/examplemockspy/ExampleMockSpyApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.examplemockspy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleMockSpyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleMockSpyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/examplemockspy/ExampleMockSpyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.examplemockspy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleMockSpyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
