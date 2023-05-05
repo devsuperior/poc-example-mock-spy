95ed6ee91bb12cbf9d774f33bb03271494550a4f
oliveiralex <alexandre.si.ufu@gmail.com>
2023-05-05 11:05:30 -0300

    Implementing unit tests

---
+++ src/test/java/com/devsuperior/examplemockspy/services/ProductServiceTests.java
@@ -0,0 +1,166 @@
+package com.devsuperior.examplemockspy.services;
+
+import static org.mockito.ArgumentMatchers.any;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.devsuperior.examplemockspy.dto.ProductDTO;
+import com.devsuperior.examplemockspy.entities.Product;
+import com.devsuperior.examplemockspy.repositories.ProductRepository;
+import com.devsuperior.examplemockspy.services.exceptions.InvalidDataException;
+import com.devsuperior.examplemockspy.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+	
+	@InjectMocks
+	private ProductService service;
+	
+	@Mock
+	private ProductRepository repository;
+	
+	private long existingProductId, nonExistingProductId;
+	private Product product;
+	private ProductDTO productDTO;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingProductId = 1L;
+		nonExistingProductId = 2L;
+		
+		product = new Product(1L, "Playstation", 10.0);
+		productDTO = new ProductDTO(product);
+		
+		Mockito.when(repository.save(any())).thenReturn(product);
+		
+		Mockito.when(repository.getReferenceById(existingProductId)).thenReturn(product);
+		Mockito.when(repository.getReferenceById(nonExistingProductId)).thenThrow(EntityNotFoundException.class);
+	}
+	
+	@Test
+	public void insertShouldReturnProductDTOWhenValidData() {
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doNothing().when(serviceSpy).validateData(productDTO);
+		
+		ProductDTO result = service.insert(productDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getName(), "Playstation");
+	}
+	
+	@Test
+	public void insertShouldReturnInvalidDataExceptionWhenProductNameIsBlank() {
+		
+		productDTO.setName("");
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+		
+		Assertions.assertThrows(InvalidDataException.class, () -> {
+	    	@SuppressWarnings("unused")
+	    	ProductDTO result = service.insert(productDTO);
+		});
+	}
+	
+	@Test
+	public void insertShouldReturnInvalidDataExceptionWhenProductPriceIsNegative() {
+		
+		productDTO.setPrice(-5.0);
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+		
+		Assertions.assertThrows(InvalidDataException.class, () -> {
+	    	@SuppressWarnings("unused")
+	    	ProductDTO result = service.insert(productDTO);
+		});
+	}
+	
+	@Test
+	public void updateShouldReturnProductDTOWhenIdExistsAndValidData() {
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doNothing().when(serviceSpy).validateData(productDTO);
+		
+		ProductDTO result = service.update(existingProductId, productDTO);
+		
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(result.getId(), existingProductId);	
+	}
+	
+	@Test
+	public void updateShouldReturnInvalidDataExceptionWhenIdExistsAndProductNameIsBlank() {
+		
+		productDTO.setName("");
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+		
+		Assertions.assertThrows(InvalidDataException.class, () -> {
+	    	@SuppressWarnings("unused")
+	    	ProductDTO result = service.update(existingProductId, productDTO);
+		});	
+	}
+	
+	@Test
+	public void updateShouldReturnInvalidDataExceptionWhenIdExistsAndPriceIsNegative() {
+		
+		productDTO.setPrice(-5.0);
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+		Assertions.assertThrows(InvalidDataException.class, () -> {
+	    	@SuppressWarnings("unused")
+	    	ProductDTO result = service.update(existingProductId, productDTO);
+		});	
+	}
+	
+	@Test
+	public void updateShouldReturnResourceNotFoundExceptionWhenIdDoesNotExistAndValidData() {
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doNothing().when(serviceSpy).validateData(productDTO);
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.update(nonExistingProductId, productDTO);
+		});	
+	}
+	
+	@Test
+	public void updateShouldReturnInvalidDataExceptionWhenIdDoesNotExistAndNameIsBlank() {
+		
+		productDTO.setName("");
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+		
+		Assertions.assertThrows(InvalidDataException.class, () -> {
+			service.update(nonExistingProductId, productDTO);
+		});	
+	}
+	
+	@Test
+	public void updateShouldReturnInvalidDataExceptionWhenIdDoesNotExistAndPriceIsNegative() {
+		
+		productDTO.setPrice(-5.0);
+		
+		ProductService serviceSpy = Mockito.spy(service);
+		Mockito.doThrow(InvalidDataException.class).when(serviceSpy).validateData(productDTO);
+
+		
+		Assertions.assertThrows(InvalidDataException.class, () -> {
+			service.update(nonExistingProductId, productDTO);
+		});	
+	}	
+}
